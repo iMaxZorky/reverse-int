2c8adb88fec25e65d9753489489c3f65f52d5d33
iMaxZorky <mkoprav@gmail.com>
2022-12-30 22:22:46 +0600

    update: all done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n *= -1;
+  }
+  let arr1   = String(n).split('');
+  let arr2   = arr1.reverse();
+  let result = arr2.join('');
+  return result
 }
